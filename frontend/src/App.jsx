ec3582dc6029699664fa149e5cd55aaa62d2a47a
rileytittle <coletittle@ymail.com>
2024-10-03 16:59:18 -0400

    Fix routing in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,15 +11,12 @@ import Homepage from "./pages/Homepage";
 import HomeownerApplication from "./pages/HomeownerApplication";
 import CreateVolunteer from "./pages/CreateVolunteer";
 import ConfirmVolunteer from "./pages/ConfirmVolunteer";
-import Login from './pages/login/Login';
+import Login from "./pages/login/Login";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<MainLayout />}>
-			<Route
-				path="/home"
-				element={<Homepage />}
-			></Route>
+			<Route path="/home" element={<Homepage />}></Route>
 			<Route
 				path="/create-volunteer"
 				element={<CreateVolunteer />}
@@ -28,13 +25,11 @@ const router = createBrowserRouter(
 				path="/request-help"
 				element={<HomeownerApplication />}
 			></Route>
+			<Route
 				path="/create-volunteer/confirm"
 				element={<ConfirmVolunteer />}
 			></Route>
-      <Route
-        path="/login"
-        element={<Login />}
-      ></Route>
+			<Route path="/login" element={<Login />}></Route>
 		</Route>
 	)
 );
