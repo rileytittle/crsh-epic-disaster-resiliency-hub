e893defdd490472f37cf3d2ff2498598552a45a1
Riley Tittle <rileytittle02@gmail.com>
2024-10-03 17:00:54 -0400

    Fix routing in app.jsx (#3)

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const admin_route_1 = require("./routes/admin.route");
+const homeowner_route_1 = require("./routes/homeowner.route");
 let cors = require("cors");
 let app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -12,4 +13,5 @@ app.use(cors({
     origin: "http://localhost:5173", // Allow only this origin
 }));
 app.use("/admin", admin_route_1.app);
+app.use("/homeowner", homeowner_route_1.app);
 app.listen(3000);
