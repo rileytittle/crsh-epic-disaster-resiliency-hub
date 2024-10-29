0d3230de5e23a5ef2224687c7ecebe79d5a07a22
haydeno2 <131286490+haydeno2@users.noreply.github.com>
2024-10-29 15:55:18 -0400

    login is no longer gone

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "../../App.css";
+
+function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoggedin, setIsLoggedin] = useState(false);
+
+    const login = (e) => {
+        e.preventDefault();
+        const userData = {
+            username,
+            password,
+        };
+
+        if (username === 'guest' && password === 'pass') {
+            setIsLoggedin(true);
+            localStorage.setItem("token-info", JSON.stringify(userData));
+            setUsername("");
+            setPassword("");
+        } else {
+            alert("Invalid credentials");
+        }
+    };
+
+    const logout = () => {
+        localStorage.removeItem("token-info");
+        setIsLoggedin(false);
+    };
+
+    return (
+        <>
+            <div style={{ textAlign: "center" }}>
+
+                {!isLoggedin ? (
+                    <>
+                        <h1>Use 'guest' and 'pass' to log in</h1>
+                        <form action="">
+
+                            <input
+                                type="text"
+                                onChange={(e) =>
+                                    setUsername(e.target.value)
+                                }
+                                value={username}
+                                placeholder="Username"
+                            />
+                            <input
+                                type="password"
+                                onChange={(e) =>
+                                    setPassword(
+                                        e.target.value
+                                    )
+                                }
+                                value={password}
+                                placeholder="Password"
+                            />
+                            <button
+                                type="submit"
+                                onClick={login}
+                            >
+                                GO
+                            </button>
+                        </form>
+                    </>
+                ) : (
+                    <>
+                        <h1>User is logged in</h1>
+                        <button onClickCapture={logout}>
+                            logout user
+                        </button>
+                    </>
+                )}
+            </div>
+        </>
+    );
+}
+
+export default Login;
