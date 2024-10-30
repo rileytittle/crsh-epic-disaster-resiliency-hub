44adb4ca65da6d73b14cf4814f581893821b58f3
PenguinSquire <syates03@gmail.com>
2024-10-30 00:38:59 -0400

    fixed backend folder pathing

--- backend/dist/Routes/Volunteer.route.js
+++ backend/dist/Routes/Volunteer.route.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-
 exports.app = void 0;
 const express_1 = require("express");
 const volunteerApplication_model_1 = require("../models/volunteerApplication.model");
@@ -8,7 +7,6 @@ const app = (0, express_1.Router)();
 exports.app = app;
 let volunteerApplications = []; // Temporary storage of applications
 app.post("/create", (req, res) => {
-
     const { firstName, lastName, phoneNumber, email, streetAddress, city, state, zipCode, areasOfHelp } = req.body;
     const id = Math.floor(Math.random() * 10000);
     // Create a new instance of VolunteerApplication
@@ -17,4 +15,3 @@ app.post("/create", (req, res) => {
     volunteerApplications.push(newVolunteer);
     res.status(201).json({ message: 'Volunteer Application Created', volunteer: newVolunteer });
 });
-
