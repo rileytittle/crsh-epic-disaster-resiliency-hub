5485d8e9f30b89fa62ac9086f5b82e913cd63cd5
PenguinSquire <84363175+PenguinSquire@users.noreply.github.com>
2024-10-30 17:08:38 -0400

    remove duplicate

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,6 @@ import { app as AdminRouter } from "./routes/admin.route";
 import { app as helpRouter } from "./routes/homeowner.route";
 import {app as volunteerRouter} from "./routes/Volunteer.route";
 
-import {app as volunteerRouter} from "./routes/Volunteer.route";
-
 
 let cors = require("cors");
 let app = express();
