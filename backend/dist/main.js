e042b0a4c4c3bd041a28826a5aabe144910a5f7a
rileytittle <coletittle@ymail.com>
2024-12-05 15:54:57 -0500

    another msg

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const admin_route_1 = require("./Routes/admin.route");
-const homeowner_route_1 = require("./Routes/homeowner.route");
-const Volunteer_route_1 = require("./Routes/Volunteer.route");
+const admin_route_1 = require("./routes/admin.route");
+const homeowner_route_1 = require("./routes/homeowner.route");
+const homeowner_route_2 = require("./routes/homeowner.route");
 let cors = require("cors");
 let app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -15,5 +15,5 @@ app.use(cors({
 }));
 app.use("/admin", admin_route_1.app);
 app.use("/homeowner", homeowner_route_1.app);
-app.use("/volunteer", Volunteer_route_1.app);
+app.use("/volunteer", homeowner_route_2.app);
 app.listen(3000);
