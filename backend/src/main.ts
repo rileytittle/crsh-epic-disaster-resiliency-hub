cd5b25d27c684c71b0d030531a5db1eff16bef3b
rileytittle <coletittle@ymail.com>
2024-12-08 15:58:14 -0500

    add

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,20 @@
 import express from "express";
 import { app as AdminRouter } from "./routes/admin.route";
 import { app as helpRouter } from "./routes/homeowner.route";
-import {app as volunteerRouter} from "./routes/Volunteer.route";
+import { app as volunteerRouter } from "./routes/Volunteer.route";
 
 let cors = require("cors");
 let app = express();
 app.use(express.json());
 
-app.use(
-	cors({
-		origin: "http://localhost:5173", // Allow only this origin
-	})
-);
-
+app.get("/", (req, res) => {
+	try {
+		//write some logic here
+		res.status(200).send("Your app deployed!");
+	} catch (e) {
+		res.status(500).send(e);
+	}
+});
 app.use("/admin", AdminRouter);
 app.use("/homeowner", helpRouter);
 app.use("/volunteer", volunteerRouter);
