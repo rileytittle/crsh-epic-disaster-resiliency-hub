cd5418d1736826cede36aacbb9420a968b1c306b
rileytittle <coletittle@ymail.com>
2025-04-01 12:24:49 -0400

    change to prod mode

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -13,10 +13,10 @@ const ExcelJS = require("exceljs");
 const fs = require("fs");
 const path = require("path");
 let saltRounds = 10;
-require('dotenv').config();
+require("dotenv").config();
 const SECRET_KEY =
 	"0fb5f53f4d7ae5114979d94d01ddf11bf7e11d30dadf025732642995194fdf5fa0e62d5f726de0315e09c780319f98e512dc3c3a6c0ea8c847e7f1e76885bcd0";
-const IN_DEVELOPMENT = true;
+const IN_DEVELOPMENT = false;
 let pool: Pool;
 
 if (IN_DEVELOPMENT) {
@@ -134,12 +134,12 @@ app.post("/login", async (req, res) => {
 						});
 						if (result) {
 							let token = jwt.sign(
-								{ 
-									email: user.email, 
+								{
+									email: user.email,
 									isAdmin: true,
 									userType: "admin",
 									firstName: user.first_name,
-									lastName: user.last_name
+									lastName: user.last_name,
 								},
 								SECRET_KEY
 							);
@@ -179,7 +179,7 @@ app.post("/create-volunteer/accept", async (req, res) => {
 			[
 				application.rows[0].email,
 				application.rows[0].first_name[0] +
-				application.rows[0].last_name,
+					application.rows[0].last_name,
 				null,
 				application.rows[0].first_name,
 				application.rows[0].last_name,
@@ -269,40 +269,40 @@ app.get("/assign-volunteer/list", async (req, res) => {
 	try {
 		// Query the VolunteerAccount table
 		const result = await pool.query('SELECT * FROM "volunteer"');
-		
+
 		// Map the results to Volunteer instances
 		const volunteers = result.rows.map((row) => {
-		  const areasOfHelp = [];
-		  if (row.admin_team) areasOfHelp.push('Admin Team');
-		  if (row.hospitality) areasOfHelp.push('Hospitality');
-		  if (row.logistic_tracking) areasOfHelp.push('Logistics');
-		  if (row.community_outreach) areasOfHelp.push('Community Outreach');
-		  if (row.community_helpers) areasOfHelp.push('Community Helpers');
-	
-		  return new Volunteer(
-			row.id,
-			row.first_name,
-			row.last_name,
-			row.phone_number,
-			row.email,
-			row.street_address,
-			row.street_address2,
-			row.city,
-			row.state,
-			row.zip_code,
-			areasOfHelp,
-			row.team_leader, // Assuming team_leader is a boolean column in your table
-			row.password
-		  );
+			let areasOfHelp: string[] = [];
+			if (row.admin_team) areasOfHelp.push("Admin Team");
+			if (row.hospitality) areasOfHelp.push("Hospitality");
+			if (row.logistic_tracking) areasOfHelp.push("Logistics");
+			if (row.community_outreach) areasOfHelp.push("Community Outreach");
+			if (row.community_helpers) areasOfHelp.push("Community Helpers");
+
+			return new Volunteer(
+				row.id,
+				row.first_name,
+				row.last_name,
+				row.phone_number,
+				row.email,
+				row.street_address,
+				row.street_address2,
+				row.city,
+				row.state,
+				row.zip_code,
+				areasOfHelp,
+				row.team_leader, // Assuming team_leader is a boolean column in your table
+				row.password
+			);
 		});
-	
+
 		// Return the array of Volunteer instances
 		res.json(volunteers);
-	  } catch (err) {
-		console.error('Error fetching volunteers:', err);
-		res.status(500).json({ error: 'Failed to fetch volunteers' });
-	  }
-	});
+	} catch (err) {
+		console.error("Error fetching volunteers:", err);
+		res.status(500).json({ error: "Failed to fetch volunteers" });
+	}
+});
 app.patch("/volunteers/volunteer-details", async (req, res) => {
 	try {
 		// Ensure `areaToChange` is a valid column name.
