a4036d754ae20cb4fcbbcfe56b5541f730a3748c
rileytittle <coletittle@ymail.com>
2025-03-31 21:04:25 -0400

    get rid of console log

--- frontend/src/pages/AdminLogin/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin/AdminLogin.jsx
@@ -20,7 +20,6 @@ function AdminLogin() {
 			let headers = {
 				Authorization: `Basic ${basicAuthHeader}`,
 			};
-			console.log(import.meta.env.VITE_API_URL);
 			await axios
 				.post(`${import.meta.env.VITE_API_URL}/admin/login`, userData, {
 					headers,
