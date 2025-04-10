b322199b024d23ac0e15f11083df6c3cc2dbcb66
rileytittle <coletittle@ymail.com>
2025-04-09 20:56:05 -0400

    Address EPIC concerns

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,18 +1,53 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
-
-function Homepage() {
+import "./Homepage.css";
+export default function Homepage() {
 	return (
 		<>
-			<div className="d-flex justify-content-center align-items-center vh-100">
-				<img
-					src="/EPIC-logo.png" // Replace with your image URL
-					alt="Displayed"
-					className="img-fluid rounded shadow-lg"
-				/>
+			<div className="text-center mt-4">
+				<h1 className="display-4 text-primary">
+					EPIC Disaster Resiliency Hub
+				</h1>
+			</div>
+			<div className="d-flex flex-column align-items-center">
+				<div className="image-container">
+					<img
+						src="/EPIC-logo.png"
+						alt="Displayed"
+						className="img-fluid rounded scaled-image"
+					/>
+				</div>
+				<div className="card-container">
+					<div className="card rounded-3 shadow-lg">
+						<div className="card-body">
+							<div className="text-center">
+								<h2 className="mb-3 text-primary">
+									Our Mission
+								</h2>
+								<p className="fs-5 text-muted">
+									During day-to-day operations, known as{" "}
+									<strong>Blue Skys</strong>, EPIC is a center
+									providing partner resources with programs
+									and services that educate, support, and
+									promote mental well-being for individuals
+									struggling physically, emotionally, and
+									spiritually.
+								</p>
+								<p className="fs-5 text-muted">
+									After a disaster, known as{" "}
+									<strong>Gray Skys</strong>, EPIC, in
+									partnership with the American Red Cross,
+									transitions to a Disaster Resilience Hub
+									serving the Southern Sarasota and Western
+									Charlotte Counties in Florida. EPIC will be
+									prepared to take action to inform the
+									community about the current weather advisory
+									or act in response to a major disaster
+									impact by beginning recovery efforts.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
 }
-export default Homepage;
--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -9,7 +9,14 @@ function Resources() {
 					<div className="card shadow-sm">
 						<div className="card-body">
 							<h5 className="card-title">NOAA</h5>
-							<p className="card-text">NOAA</p>
+							<p className="card-text">
+								From daily weather forecasts, severe storm
+								warnings, and climate monitoring to fisheries
+								management, coastal restoration and supporting
+								marine commerce, NOAA’s products and services
+								support economic vitality and affect more than
+								one-third of America’s gross domestic product.{" "}
+							</p>
 							<a
 								href="https://www.noaa.gov/"
 								target="_blank"
@@ -21,7 +28,33 @@ function Resources() {
 						</div>
 					</div>
 				</div>
-
+				<div className="col-md-6 mb-3">
+					<div className="card shadow-sm">
+						<div className="card-body">
+							<h5 className="card-title">
+								Federal Emergency Management Agency (FEMA)
+							</h5>
+							<p className="card-text">
+								The Federal Emergency Management Agency (FEMA)
+								employs more than 20,000 people nationwide.
+								Headquartered in Washington, D.C., we have 10
+								regional offices located across the country. We
+								leverage a tremendous capacity to coordinate
+								within the federal government to make sure
+								America is equipped to prepare for and respond
+								to disasters.
+							</p>
+							<a
+								href="https://www.fema.gov/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="btn btn-primary"
+							>
+								Visit Site
+							</a>
+						</div>
+					</div>
+				</div>
 				<div className="col-md-6 mb-3">
 					<div className="card shadow-sm">
 						<div className="card-body">
@@ -29,7 +62,11 @@ function Resources() {
 								National Weather Service
 							</h5>
 							<p className="card-text">
-								National Weather Service
+								Provide weather, water and climate data,
+								forecasts, warnings, and impact-based decision
+								support services for the protection of life and
+								property and enhancement of the national
+								economy.
 							</p>
 							<a
 								href="https://www.weather.gov/"
@@ -49,7 +86,10 @@ function Resources() {
 							<h5 className="card-title">
 								Live Hurricane Tracker
 							</h5>
-							<p className="card-text">Live Hurricane Tracker</p>
+							<p className="card-text">
+								A real-time live hurricane tracker provided by
+								NOAA
+							</p>
 							<a
 								href="https://www.nesdis.noaa.gov/imagery/hurricanes/live-hurricane-tracker"
 								target="_blank"
@@ -68,9 +108,7 @@ function Resources() {
 							<h5 className="card-title">
 								Sarasota County Emergency Operations Center
 							</h5>
-							<p className="card-text">
-								Sarasota County Emergency Operations Center
-							</p>
+							<p className="card-text"></p>
 							<a
 								href="https://www.scgov.net/"
 								target="_blank"
@@ -88,9 +126,7 @@ function Resources() {
 							<h5 className="card-title">
 								Charlotte County Emergency Operations Center
 							</h5>
-							<p className="card-text">
-								Charlotte County Emergency Operations Center
-							</p>
+							<p className="card-text"></p>
 							<a
 								href="https://www.charlottecountyfl.gov/departments/public-safety/emergency-management/"
 								target="_blank"
@@ -102,35 +138,14 @@ function Resources() {
 						</div>
 					</div>
 				</div>
-				<div className="col-md-6 mb-3">
-					<div className="card shadow-sm">
-						<div className="card-body">
-							<h5 className="card-title">
-								Federal Emergency Management Agency (FEMA)
-							</h5>
-							<p className="card-text">
-								Federal Emergency Management Agency (FEMA)
-							</p>
-							<a
-								href="https://www.fema.gov/"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="btn btn-primary"
-							>
-								Visit Site
-							</a>
-						</div>
-					</div>
-				</div>
+
 				<div className="col-md-6 mb-3">
 					<div className="card shadow-sm">
 						<div className="card-body">
 							<h5 className="card-title">
 								W.I.N.K. The Weather Authority
 							</h5>
-							<p className="card-text">
-								W.I.N.K. The Weather Authority
-							</p>
+							<p className="card-text"></p>
 							<a
 								href="https://winknews.com/weather/"
 								target="_blank"
@@ -146,7 +161,7 @@ function Resources() {
 					<div className="card shadow-sm">
 						<div className="card-body">
 							<h5 className="card-title">ABC7 WWSB</h5>
-							<p className="card-text">ABC7 WWSB</p>
+							<p className="card-text"></p>
 							<a
 								href="https://www.mysuncoast.com/"
 								target="_blank"
