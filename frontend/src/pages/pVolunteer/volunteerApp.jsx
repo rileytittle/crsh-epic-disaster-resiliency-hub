6ef535ef8378c3d556e2f0a5128b57d9c338f43d
Corey Sorelle <CoreyR.Sorelle@hotmail.com>
2025-04-10 21:14:33 -0400

    fixed line

--- frontend/src/pages/pVolunteer/volunteerApp.jsx
+++ frontend/src/pages/pVolunteer/volunteerApp.jsx
@@ -156,7 +156,7 @@ const VolunteerForm = () => {
 		}
 
 		try {
-			const response = await fetch(`http://localhost:3000/volunteer/create`, { //${import.meta.env.VITE_API_URL}
+			const response = await fetch(`${import.meta.env.VITE_API_URL}/volunteer/create`, { 
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
