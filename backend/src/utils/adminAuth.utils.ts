b0580d798c2cd17e8600312046b87a5627f06582
rileytittle <coletittle@ymail.com>
2025-04-24 11:55:18 -0400

    turn secret key into env variables for security

--- backend/src/utils/adminAuth.utils.ts
+++ backend/src/utils/adminAuth.utils.ts
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
-const SECRET_KEY =
-	"0fb5f53f4d7ae5114979d94d01ddf11bf7e11d30dadf025732642995194fdf5fa0e62d5f726de0315e09c780319f98e512dc3c3a6c0ea8c847e7f1e76885bcd0";
+const SECRET_KEY = process.env.SECRET_KEY
 let AdminAuthChecker = (req: Request, res: Response, next: NextFunction) => {
 	if (req.headers["authorization"]) {
 		let header = req.headers["authorization"];
